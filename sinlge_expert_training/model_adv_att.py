fixed the L2 gradient problem.

sinlge_expert_training/model_adv_att.py
@@ -5,7 +5,7 @@ Created on Tue Aug 11 19:23:17 2020
 
 @author: chad
 """
-
+from attack_steps import L2Step, LinfStep
 import torch
 import torch.nn as nn
 import numpy as np
@@ -50,7 +50,6 @@ def project(x, original_x, epsilon, _type='linf'):
 
     return x
 
-# Model
 class AttackPGD(nn.Module):  # change here to build l_2 and l_inf
     def __init__(self, basic_net, config):
         super(AttackPGD, self).__init__()
@@ -61,31 +60,77 @@ class AttackPGD(nn.Module):  # change here to build l_2 and l_inf
         self.num_steps = config['num_steps']
         self._type = config['_type']
         assert config['loss_func'] == 'xent', 'Only xent supported for now.'
-
-    def forward(self, inputs, targets, attack):
+        self.targeted = False 
+        self.random_start = self.rand
+    def forward(self, inputs, targets, attack, y_targets=None):
         if not attack:
-            return self.basic_net(inputs), inputs
+            return self.basic_net(inputs),  inputs
+        step = None
+        if self._type =='l2':
+            step = L2Step(eps=self.epsilon, orig_input=inputs, step_size=self.step_size) 
+        elif self._type =='linf':
+            step = LinfStep(eps=self.epsilon, orig_input=inputs, step_size=self.step_size)
+        else:
+            NotImplementedError 
+        x = inputs.clone().detach().requires_grad_(True)
+        
+        # Random start (to escape certain types of gradient masking)
+        if self.random_start:
+                x = step.random_perturb(x)
+        criterion = torch.nn.CrossEntropyLoss(reduction='none')
+        m = -1 if self.targeted else 1
+        for _ in range(self.num_steps):
+            x = x.clone().detach().requires_grad_(True)
+            outputs = self.basic_net(x)
+            losses = criterion(outputs, targets if not self.targeted else y_targets)
+            assert losses.shape[0]==x.shape[0], 'shape of losses needs to match input'
+            loss = torch.mean(losses)
+            grad, = torch.autograd.grad(m * loss, [x])
+            with torch.no_grad():
+                x = step.step(x,grad)
+                x = step.project(x)
+        ret = x.clone().detach()
+        return ret
 
-        x = inputs.detach()
-        if self.rand:     # attack here l_inf attack
-            x = x + torch.zeros_like(x).uniform_(-self.epsilon, self.epsilon)
-        for i in range(self.num_steps):
-            x.requires_grad_()
-            with torch.enable_grad():
-                logits = self.basic_net(x)
-                #loss = F.cross_entropy(logits, targets, size_average=False)
-                loss = F.cross_entropy(logits, targets, reduction='sum')
-            grad = torch.autograd.grad(loss, [x])[0]
-            x = x.detach() + self.step_size*torch.sign(grad.detach())
+
+
+
+# # Model
+# class AttackPGD(nn.Module):  # change here to build l_2 and l_inf
+#     def __init__(self, basic_net, config):
+#         super(AttackPGD, self).__init__()
+#         self.basic_net = basic_net
+#         self.rand = config['random_start']
+#         self.step_size = config['step_size']
+#         self.epsilon = config['epsilon']
+#         self.num_steps = config['num_steps']
+#         self._type = config['_type']
+#         assert config['loss_func'] == 'xent', 'Only xent supported for now.'
+
+#     def forward(self, inputs, targets, attack):
+#         if not attack:
+#             return self.basic_net(inputs), inputs
+
+#         x = inputs.detach()
+#         if self.rand:     # attack here l_inf attack
+#             x = x + torch.zeros_like(x).uniform_(-self.epsilon, self.epsilon)
+#         for i in range(self.num_steps):
+#             x.requires_grad_()
+#             with torch.enable_grad():
+#                 logits = self.basic_net(x)
+#                 #loss = F.cross_entropy(logits, targets, size_average=False)
+#                 loss = F.cross_entropy(logits, targets, reduction='sum')
+#             grad = torch.autograd.grad(loss, [x])[0]
+#             x = x.detach() + self.step_size*torch.sign(grad.detach())
            
-           # normal l_infite
-           # x = torch.min(torch.max(x, inputs - self.epsilon), inputs + self.epsilon)
+#            # normal l_infite
+#            # x = torch.min(torch.max(x, inputs - self.epsilon), inputs + self.epsilon)
             
-            x = project(x, inputs, self.epsilon, self._type)
-            x = torch.clamp(x, 0, 1)
+#             x = project(x, inputs, self.epsilon, self._type)
+#             x = torch.clamp(x, 0, 1)
 
-        #return self.basic_net(x), x
-        return x
+#         #return self.basic_net(x), x
+#         return x
 
 
 